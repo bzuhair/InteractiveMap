1fdbd985f1910f35e01e57046d936dc86f22ae75
Balal Zuhair <balalzuhair@Balals-MacBook-Pro.local>
2017-11-01 19:31:25 -0700

    configured webpack added client and server folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
+const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'react-dom',
+];
 
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
-  ],
+  entry: {
+    bundle: './client/main.js',
+    vendor: VENDOR_LIBS,
+  },
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        use: 'babel-loader',
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
       },
     ],
   },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+    }),
+  ],
 };
