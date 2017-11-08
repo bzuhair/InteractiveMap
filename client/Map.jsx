8768d388563a280fddd297fbb365ae1578de4592
Balal Zuhair <balalzuhair@balals-mbp.gateway.pace.com>
2017-11-07 22:54:58 -0800

    rendering map of SF

--- client/Map.jsx
+++ client/Map.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
-const Map = () => <div> This is the Map component</div>;
+const Map = () => {
+  const GettingStartedGoogleMap = withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
+    />
+  ),
+  );
+
+  return (
+    <GettingStartedGoogleMap
+      containerElement={
+        <div style={{ height: '1000px', width: '1000px' }} />
+      }
+
+      mapElement={
+        <div style={{ height: '100%', width: '100%' }} />
+      }
+
+    />
+  );
+};
 
 export default Map;
