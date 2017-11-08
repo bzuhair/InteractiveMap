6cbba6eaa32f604d9f5654201a2090dbcf335fc6
Balal Zuhair <balalzuhair@balals-mbp.gateway.pace.com>
2017-11-08 10:55:10 -0800

    one polyline

--- client/Map.jsx
+++ client/Map.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Polyline } from 'react-google-maps';
+import data from '../trips/2016-07-02--11-56-24.json';
+
 
 const Map = () => {
-  const GettingStartedGoogleMap = withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
-    />
+  const coordinates = data.coords.map(position => (
+    { lat: position.lat, lng: position.lng }
   ),
   );
 
+  const GettingStartedGoogleMap = withGoogleMap(() => (
+    <div>
+      <GoogleMap
+        defaultZoom={11}
+        defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
+      />
+      <Polyline
+        path={coordinates}
+      />
+    </div>
+  ),
+  );
   return (
     <GettingStartedGoogleMap
       containerElement={
